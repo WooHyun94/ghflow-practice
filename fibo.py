cf42763d6b7dc7c6f58b6632e1c0587902111ec1
WooHyun94 <94dngus@gmail.com>
2022-09-23 15:49:15 +0900

    feat : Print fibo
    Print fibo used memoyzation.

--- fibo.py
+++ fibo.py
@@ -1,6 +1,10 @@
+dic = {0:0, 1:1}
+
 def fibo(n):
-    if n <= 2:
-        return 1
-    return fibo(n - 1) + fibo(n - 2)
+    global dic
+    if n in dic:
+        return dic[n]
+    dic[n] = fibo(n - 1) + fibo(n - 2)
+    return dic[n]
 
 print(fibo(5))
